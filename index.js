0775bd1f9e082c6bf64d7eb7f689f6c8d4be6b26
Muhammad Khubaib <muhammadkhubaib492@gmail.com>
2024-03-23 14:48:22 +0500

    i have completed my recipe app with json file of BBC

--- index.js
+++ index.js
@@ -6,25 +6,81 @@
     
     const searchUlRecipes = document.querySelector("#recipeList");
 
+    const detailsRecipes = (response) =>{
+
+        recipeDetailsContainer.innerHTML=""
+        let div = document.createElement("div")
+        div.className = "recipeDetailsDiv"
+        let recipeDetails =
+        `
+        <h2>${response.title}</h2>
+
+        <h3>Ingredients</h3>
+
+        <ul>${response.ingredients.map((ingredients)=>{
+            return `<li>${ingredients}</li>`
+        }).join('')}
+        </ul>
+
+        <h3>Instruction</h3>
+
+        <ol>
+        ${response.instructions.map((instruction)=>{
+            return `<li>${instruction}</li> </br> `
+        }).join('')}
+        </ol>
+        `
+        div.innerHTML = recipeDetails 
+        recipeDetailsContainer.appendChild(div)
+    }
 
     window.searchRecipes = (filteredRecipes) => {
         searchUlRecipes.innerHTML="";    
         filteredRecipes.map((response) => {
             let li = document.createElement("li");
             const liDiv = `
-             <div>${response.title}</div>
+            <div>
+            <h3>${response.title}.</h3> 
+            <div>${response.description}</div>
+            </div>
             `;
             li.innerHTML = liDiv
+            li.addEventListener("click",()=>{
+                detailsRecipes(response)
+            })
             searchUlRecipes.appendChild(li); 
         });
     }
     
     window.search = () => {
         const userInput = document.querySelector("#searchInput").value;
+
+        if (userInput === "") {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Please enter a search term',
+            });
+            return
+        }
+
         const filteredRecipes = recipes.filter((response) => {
             return response.title.toLowerCase().includes(userInput.toLowerCase()) ||
             response.ingredients.join('').toLowerCase().includes(userInput.toLowerCase());
         })
+        if (filteredRecipes.length === 0){
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Recipe Is Not Found',
+            });
+        }else{
+            Swal.fire({
+                icon: 'success',
+                title: 'Hurray!',
+                text:'Recipes are Found'
+            })
+        }
         searchRecipes(filteredRecipes); 
     }
     inputBtn.addEventListener("click", search);         
